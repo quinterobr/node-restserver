4bc4c4d2bee9d10f5070df99ef15617516067e3b
brahian quintero <brahianquintero7@gmail.com>
2020-12-18 13:41:46 -0500

    variable entorno cadena conexion mongo

--- server/config/config.js
+++ server/config/config.js
@@ -8,6 +8,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://bsqg11:lCt15pXXVCbR9sJC@cluster0.7civu.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
